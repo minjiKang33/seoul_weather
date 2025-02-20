d43cdb73605ff10d9d5cd9aa32a27552900c3a33
minjiKang33 <gna97801@gmail.com>
2025-02-20 11:50:17 +0900

    Create csv2html.py

---
+++ csv2html.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, Response
+
+
+app = Flask(__name__)
+@app.route("/")
+
+@app.route("/csv")
+def print_csv():
+
+    import pandas as pd
+
+    df = pd.read_csv()
+    dataframe = df.to_html()
+
+    return render_template("seoul.html", dataframe=dataframe)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
+
